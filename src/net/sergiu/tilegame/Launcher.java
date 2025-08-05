2a571d28d8628c1add59dfaa31679944920507e0
Sergiu Ungureanu <sergiu.ungureanu2005@gmail.com>
2025-08-05 14:45:15 +0300

    Initial import of TileGame

---
+++ src/net/sergiu/tilegame/Launcher.java
@@ -0,0 +1,10 @@
+package net.sergiu.tilegame;
+
+import net.sergiu.tilegame.display.Display;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        new Game("Title", 800, 800);
+    }
+}
